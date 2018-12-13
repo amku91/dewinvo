7c8bbc22a3baab610a27e3d04b0c7bde8838c1a0
amku91 <amit.kumar@wellthy.care>
2018-12-13 10:17:37 +0530

    Database file update

--- app/Config/database.php
+++ app/Config/database.php
@@ -71,10 +71,10 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' 		 => 'localhost',
-		'login'      => 'root',
+		'host' 		 => '',
+		'login'      => '',
 		'password'   => '',
-		'database'   => 'trial',
+		'database'   => '',
 		'prefix'     => '',
 		'encoding'   => 'utf8',
 	);
